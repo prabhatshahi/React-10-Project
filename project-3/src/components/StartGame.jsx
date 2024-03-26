272da9ad8fe6846855f477f8eae325548c8e5e47
Prabhat Ranjan Shahi <ranjanprabhat555@gmail.com>
2024-03-26 12:10:42 +0530

    completed

--- project-3/src/components/StartGame.jsx
+++ project-3/src/components/StartGame.jsx
@@ -1,18 +1,15 @@
-import React from "react";
-
 import styled from "styled-components";
+import { Button } from "../styled/Button";
 
-const StartGame = () => {
+const StartGame = ({ toggle }) => {
   return (
     <Container>
       <div>
-        {" "}
         <img src="/images/dices.png" />
       </div>
-
       <div className="content">
         <h1>Dice Game</h1>
-        <Button>Play Now</Button>
+        <Button onClick={toggle}>Play Now</Button>
       </div>
     </Container>
   );
@@ -22,38 +19,15 @@ export default StartGame;
 
 const Container = styled.div`
   max-width: 1180px;
-  display: flex;
   height: 100vh;
-  align-items: center;
+  display: flex;
   margin: 0 auto;
+  align-items: center;
 
   .content {
     h1 {
       font-size: 96px;
       white-space: nowrap;
     }
-  }
-`;
-
-const Button = styled.button`
-  font-size: 16px;
-
-  padding: 10px 18px;
-
-  color: white;
-
-  min-width: 220px;
-  border: none;
-  background: #000000;
-  border-radius: 5px;
-  border: 1px solid transparent;
-  transition: 0.4s background ease-in;
-  cursor: pointer;
-
-  &:hover {
-    background-color: white;
-    border: 1px solid black;
-    color: black;
-    transition: 0.3s background ease-in;
   }
 `;
